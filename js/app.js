fe8be76ec44ab5bf0522af23d9ee429c5a262f48
maxkrieg <maxwell.krieg@gmail.com>
2016-08-29 22:46:15 -0400

    Start site redesign

---
+++ js/app.js
@@ -0,0 +1,33 @@
+var app = {
+  subheaderCarousel: function() {
+    var types = [
+      'Full Stack Web Developer',
+      'Skiier',
+      'Climber',
+      'Runner',
+      'Biker',
+      'Book worm',
+      'IPA addict',
+      'Coffee-holic',
+      'Dog-father'
+    ];
+    var subheaderEl = $('.header__type');
+    var currentIndex = 1;
+    var endIndex = types.length - 1;
+    var cycleTypes = setInterval(function() {
+      subheaderEl.hide().empty().append(types[currentIndex]).fadeIn(1000)
+      currentIndex += 1
+      if (currentIndex >= endIndex) {
+        currentIndex = 0
+      };
+    }, 3000);
+  },
+  init: function() {
+    app.subheaderCarousel()
+  }
+};
+
+
+$(document).ready(function() {
+  app.init()
+});
